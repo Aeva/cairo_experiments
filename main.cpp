9c1aa1e2cf476cb009d0804c2a800bf04337493b
Aeva PaleceK <aeva.ntsc@gmail.com>
2019-09-18 00:55:54 -0500

    Got Cairo and GL talking to eachother.

--- main.cpp
+++ main.cpp
@@ -8,11 +8,51 @@
 #include "glue/blob_builder.h"
 #include "main.h"
 
+#include "cairo.h"
+
 using namespace RenderingEvents;
 
 ShaderPipeline SplatShader;
-
 Buffer ScreenInfo;
+GLuint TestTexture;
+
+
+void DrawTestImage(cairo_surface_t* Surface)
+{
+	cairo_t* Ctx = cairo_create(Surface);
+	cairo_scale(Ctx, 1.0, -1.0);
+	cairo_translate(Ctx, 0.0, double(-ScreenHeight));
+	const int Samples = 30;
+	for (int i = 0; i <= Samples; ++i)
+	{
+		double a = double(i) / double(Samples);
+		if (i == Samples)
+		{
+			cairo_set_source_rgba(Ctx, 1.0, 0.5, 0.5, 1.0);
+		}
+		else
+		{
+			cairo_set_source_rgba(Ctx, a, 0.0, a * 0.7, a);
+		}
+		cairo_select_font_face(Ctx, "monospace", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+		cairo_set_font_size(Ctx, 512.0);
+		cairo_move_to(Ctx, 100.0 * a - 80.0, 200.0 * a + 500.0);
+		cairo_show_text(Ctx, "Hail Eris!");
+	}
+}
+
+void UploadSurfaceToTexture(cairo_surface_t* Surface, GLuint Texture)
+{
+	cairo_surface_flush(Surface);
+	glTextureStorage2D(Texture, 1, GL_RGBA8, ScreenWidth, ScreenHeight);
+	glTextureSubImage2D(Texture, 0, 0, 0, ScreenWidth, ScreenHeight, GL_RGBA,
+		GL_UNSIGNED_INT_8_8_8_8_REV,
+		(GLuint*)cairo_image_surface_get_data(Surface));
+	glTextureParameteri(Texture, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTextureParameteri(Texture, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTextureParameteri(Texture, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTextureParameteri(Texture, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+}
 
 
 StatusCode RenderingEvents::Setup()
@@ -32,10 +72,25 @@ StatusCode RenderingEvents::Setup()
 		ScreenInfo.Initialize((void*)&BufferData, TotalSize);
 	}
 
+	{
+		cairo_surface_t* Surface;
+		Surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, ScreenWidth, ScreenHeight);
+		DrawTestImage(Surface);
+		glCreateTextures(GL_TEXTURE_2D, 1, &TestTexture);
+		UploadSurfaceToTexture(Surface, TestTexture);
+	}
+
 	// cheese opengl into letting us draw triangles without any data
 	GLuint vao;
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
+
+	glClipControl(GL_LOWER_LEFT, GL_ZERO_TO_ONE);
+	glDisable(GL_CULL_FACE);
+	glDisable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glClearDepth(1);
+	glFrontFace(GL_CW);
 
 	return StatusCode::PASS;
 }
@@ -47,5 +102,6 @@ void RenderingEvents::Render()
 
 	SplatShader.Activate();
 	ScreenInfo.Bind(GL_UNIFORM_BUFFER, 1);
+	glBindTextureUnit(0, TestTexture);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
--- shaders/splat.fs.glsl
+++ shaders/splat.fs.glsl
@@ -6,10 +6,15 @@ uniform ScreenInfoBlock
 	vec4 ScreenSize; // (Width, Height, InvWidth, InvHeight)
 };
 
+layout(binding=0) uniform sampler2D CairoSurface;
+
 layout(location = 0) out vec4 Color;
 void main ()
 {
+	vec4 CairoColor = texelFetch(CairoSurface, ivec2(gl_FragCoord.xy), 0).bgra;
+
 	vec3 UVW = vec3(gl_FragCoord.xy * ScreenSize.zw, 0.0);
 	UVW.z = 1.0 - (UVW.x * UVW.y);
-	Color = vec4(UVW.xzy, 1.0);
+
+	Color = vec4(UVW.xzy * (1.0 - CairoColor.a) + CairoColor.rgb, 1.0);
 }
